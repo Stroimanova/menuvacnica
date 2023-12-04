8023d867535d59418d2c6fa495c47a612c09e226
unknown <simonastroimanova@hotmail.com>
2023-12-04 22:41:24 +0100

    first java program

---
+++ Menuvacnica.java
@@ -0,0 +1,11 @@
+
+public class Menuvacnica {
+
+	public static void main(String[] args) {
+	int a=65;
+	double x=a*61.7;
+	System.out.println("65evra vo denari se: ");
+	System.out.println(x);
+	}
+
+}
